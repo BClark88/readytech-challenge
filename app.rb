c596efc07c9d3cdc64cfdf1ec65c0c77f8c79837
Brady Clark <bradyjclark88@zepto.com.au>
2024-08-15 08:38:15 +0800

    update args

--- app.rb
+++ app.rb
@@ -7,14 +7,15 @@ Options = Struct.new(:jobs_csv_path, :job_seekers_csv_path)
 args = Options.new
 
 parser = OptionParser.new do |act|
-  act.on('--jobs-csv TEXT', 'path to the csv file containing the list of jobs') do |arg|
+  act.on('--jobs-csv FILE_PATH', 'path to the csv file containing the list of jobs') do |arg|
     args.jobs_csv_path = arg
   end
 
-  act.on('--job-seekers-csv [TEXT]', 'path to the csv file containing the list of job seekers') do |arg|
+  act.on('--job-seekers-csv FILE_PATH', 'path to the csv file containing the list of job seekers') do |arg|
     args.job_seekers_csv_path = arg
   end
 end
+
 parser.parse!
 
 cli_output = CLI.new.run(jobs_csv_path: args.jobs_csv_path, job_seekers_csv_path: args.job_seekers_csv_path)
